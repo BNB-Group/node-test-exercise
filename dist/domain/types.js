163b910eb182649c31f5515829a916f9539cd7d6
Sam McGoldrick <sam.mcgoldrick@bnbgroup.co.nz>
2025-07-31 08:38:00 +1200

    removed ./dist

--- dist/domain/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoanType = void 0;
-var LoanType;
-(function (LoanType) {
-    LoanType["Standard"] = "Standard";
-    LoanType["InterestOnly"] = "InterestOnly";
-    LoanType["Offset"] = "Offset";
-})(LoanType || (exports.LoanType = LoanType = {}));
--- dist/services/mortgage.service.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculate = calculate;
-function calculate(loanAmount, annualInterestRate, termYears, type, offsetAmount, addons) {
-    // TODO: Implement the actual mortgage calculation logic.
-    return 0;
-}
